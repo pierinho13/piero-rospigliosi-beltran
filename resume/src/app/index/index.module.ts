cd80e34fc5b4e3f8e3a83ea4c8f25d44cbafd9a9
pierinho13 <pierinho13@gmail.com>
2023-08-11 16:12:37 +0200

    actualizo resume

--- resume/src/app/index/index.module.ts
+++ resume/src/app/index/index.module.ts
@@ -3,9 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IndexComponent } from './index.component';
 import { DockModule } from 'primeng/dock';
 import { IndexRouting } from './index-routing.module';
+import { GalleriaModule } from 'primeng/galleria';
+import { DialogModule } from 'primeng/dialog';
+import { TreeModule } from 'primeng/tree';
+import { TerminalModule } from 'primeng/terminal';
+import { ToastModule } from 'primeng/toast';
+import { MenubarModule } from 'primeng/menubar';
+
+
+
+
 
 @NgModule({
-    imports: [ DockModule, IndexRouting  ],
+    imports: [ DockModule, DialogModule,TreeModule, TerminalModule, GalleriaModule,MenubarModule, ToastModule, IndexRouting  ],
     exports: [],
     declarations: [ IndexComponent ],
     providers: []
