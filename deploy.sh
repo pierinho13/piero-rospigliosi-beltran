5e3c2169900a3ca619e00383a4ed4b8212678c1b
pierinho13 <pierinho13@gmail.com>
2023-08-11 15:20:45 +0200

    actualizo resume

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd resume
+npm install
+npm run build
+cd ..
+cp -r resume/dist/ container
+cd container
+docker build -t pierinho13/resume:latest
+docker push
+cd ..
+git add * 
+FECHA=date '+%Y-%m-%d-%H-%M'
+git commit -m "actualizo resume $FECHA"
+git push
